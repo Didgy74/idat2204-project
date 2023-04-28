a974fccd947fd6ffc557f1ed73a89788c5d896c4
Nils Petter Skålerud <np_skalerud@hotmail.com>
2023-04-28 21:25:07 +0200

    Signed-off-by: Nils Petter Skålerud <np_skalerud@hotmail.com>

--- database.sql
+++ database.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS project;
+CREATE DATABASE project;
+USE project;
+
 CREATE TABLE users (
   id int NOT NULL UNIQUE AUTO_INCREMENT,
   PRIMARY KEY (id),
@@ -5,14 +9,28 @@ CREATE TABLE users (
   name varchar(255) NOT NULL CHECK (LENGTH(name) != 0)
 );
 
+
+
 CREATE TABLE students (
   user_id int NOT NULL UNIQUE,
   PRIMARY KEY (user_id),
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+DELIMITER //
+CREATE PROCEDURE add_student(IN real_name varchar(255), OUT student_id INT)
+BEGIN
+	INSERT INTO users VALUES(0, real_name);
+	INSERT INTO students VALUES(LAST_INSERT_ID());
+	SET student_id = LAST_INSERT_ID();
+END //
+DELIMITER ;
+
 CREATE VIEW students_info AS
 SELECT students.user_id, users.name
 FROM students JOIN users ON students.user_id = users.id;
+
+
 
 CREATE TABLE lecturers (
   user_id int NOT NULL UNIQUE,
@@ -21,6 +39,18 @@ CREATE TABLE lecturers (
   
   institute varchar(255) NOT NULL CHECK(LENGTH(institute) != 0)
 );
+
+DELIMITER //
+CREATE PROCEDURE add_lecturer(IN real_name varchar(255), IN institute varchar(255))
+BEGIN
+	INSERT INTO users VALUES(0, real_name);
+    INSERT INTO lecturers VALUES (
+		LAST_INSERT_ID(),
+        institute
+    );
+END //
+DELIMITER ;
+
 CREATE VIEW lecturers_info AS
 SELECT lecturers.user_id, users.name, lecturers.institute
 FROM lecturers JOIN users ON lecturers.user_id = users.id;
@@ -45,13 +75,21 @@ CREATE TABLE courses (
   name varchar(255) NOT NULL CHECK(LENGTH(name) != 0)
 );
 
-CREATE TABLE students_courses (
+CREATE TABLE StudentsCourses (
   student_id int NOT NULL,
   course_id int NOT NULL,
   PRIMARY KEY (student_id, course_id),
   FOREIGN KEY (student_id) REFERENCES students(user_id),
   FOREIGN KEY (course_id) REFERENCES courses(id)
 );
+
+CREATE VIEW students_courses_names AS
+SELECT courses.name AS course_name, students_info.name AS student_name
+FROM studentscourses
+JOIN students_info ON studentscourses.student_id = students_info.user_id
+JOIN courses ON studentscourses.course_id = courses.id
+ORDER BY courses.name ASC;
+
 
 CREATE TABLE bookings (
   id int NOT NULL UNIQUE AUTO_INCREMENT,
@@ -78,90 +116,11 @@ FROM bookings
 LEFT JOIN courses ON bookings.course_id = courses.id;
 
 
--- Insert a teacher user
-INSERT INTO users VALUES (
-  0,
-  "Teacher Teacherson"
-);
-INSERT INTO lecturers VALUES (
-  LAST_INSERT_ID(),
-  'IT'
-);
-
-INSERT INTO users VALUES (
-  0, 
-  'Teach Teacherman'
-);
-INSERT INTO lecturers VALUES (
-  LAST_INSERT_ID(),
-  'IT'
-);
-
-INSERT INTO users VALUES (
-  0, 
-  'Lecturer Lecturerson'
-);
-INSERT INTO lecturers VALUES (
-  LAST_INSERT_ID(),
-  'Math'
-);
-
--- Insert some students
-INSERT INTO users VALUES (
-  0, 
-  'Nils Petter'
-);
-INSERT INTO students VALUES (
-  LAST_INSERT_ID()
-);
-
-INSERT INTO users VALUES (
-  0, 
-  'Student A'
-);
-INSERT INTO students VALUES (
-  LAST_INSERT_ID()
-);
-
-INSERT INTO users VALUES (
-  0, 
-  'Student B'
-);
-INSERT INTO students VALUES (
-  LAST_INSERT_ID()
-);
-
-INSERT INTO users VALUES (
-  0, 
-  'Malin'
-);
-INSERT INTO students VALUES (
-  LAST_INSERT_ID()
-);
 
 
--- Insert some rooms
-INSERT INTO rooms VALUES (
-  0, 
-  2,
-  'A'
-);
-INSERT INTO rooms VALUES (
-  0, 
-  4,
-  'A'
-);
-INSERT INTO rooms VALUES (
-  0, 
-  4,
-  'A'
-);
-INSERT INTO rooms VALUES (
-  0, 
-  2,
-  'B'
-);
-
+CALL add_lecturer('Teacher Teacherson', 'IT');
+CALL add_lecturer('Teach Teacherman', 'IT');
+CALL add_lecturer('Lecturer Lecturerson', 'Math');
 
 
 -- Insert some fields that don't have any lecturers
@@ -204,6 +163,53 @@ INSERT INTO courses VALUE (
   0, 
   2, -- Or search a specific lecturer name
   'Accounting for Lawyers'
+);
+
+
+
+CALL add_student('Nils Petter', @student_id);
+INSERT INTO StudentsCourses VALUES (@student_id, 1);
+INSERT INTO StudentsCourses VALUES (@student_id, 2);
+INSERT INTO StudentsCourses VALUES (@student_id, 3);
+
+CALL add_student('Student A', @student_id);
+INSERT INTO StudentsCourses VALUES (@student_id, 4);
+INSERT INTO StudentsCourses VALUES (@student_id, 5);
+INSERT INTO StudentsCourses VALUES (@student_id, 6);
+
+CALL add_student('Student B', @student_id);
+INSERT INTO StudentsCourses VALUES (@student_id, 1);
+INSERT INTO StudentsCourses VALUES (@student_id, 3);
+INSERT INTO StudentsCourses VALUES (@student_id, 5);
+INSERT INTO StudentsCourses VALUES (@student_id, 7);
+
+CALL add_student('Malin', @student_id);
+INSERT INTO StudentsCourses VALUES (@student_id, 2);
+INSERT INTO StudentsCourses VALUES (@student_id, 4);
+INSERT INTO StudentsCourses VALUES (@student_id, 6);
+INSERT INTO StudentsCourses VALUES (@student_id, 8);
+
+
+-- Insert some rooms
+INSERT INTO rooms VALUES (
+  0, 
+  2,
+  'A'
+);
+INSERT INTO rooms VALUES (
+  0, 
+  4,
+  'A'
+);
+INSERT INTO rooms VALUES (
+  0, 
+  4,
+  'A'
+);
+INSERT INTO rooms VALUES (
+  0, 
+  2,
+  'B'
 );
 
 
